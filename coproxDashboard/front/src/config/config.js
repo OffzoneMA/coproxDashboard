ce0122f567bae603e507597cd6bf51346b60d8d0
youssef <youssefdiouri4@gmail.com>
2023-12-03 17:00:34 +0100

    commit

--- coproxDashboard/front/src/config/config.js
+++
@@ -1,3 +0,0 @@
-// src/config.js
-export const API_BASE_URL = 'http://localhost:8081'; // Replace with your actual API URL
-
--- coproxDashboard/server/src/routes/trelloRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const trelloController = require('../controllers/trelloController');
-
-const router = express.Router();
-
-router.post('/createTicket', trelloController.createTicket);
-router.get('/cardsWithCheckItems', trelloController.getCardsWithCheckItems);
-
-module.exports = router;
