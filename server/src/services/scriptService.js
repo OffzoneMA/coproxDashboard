d6a403ff8dba27e453041075c45051db33fb27e5
Youssef DIOURI <youssefdiouri4@gmail.com>
2024-09-16 13:59:09 +0200

    add script service edits

--- server/src/services/scriptService.js
+++ server/src/services/scriptService.js
@@ -31,7 +31,19 @@ async function updateScriptStatus(scriptName, status) {
   
 }
 
-// Get the script state by name
+async function getScriptState(scriptName) {
+  try {
+    return await connectAndExecute(async () => {
+      const scriptCollection = MongoDB.getCollection('ScriptState');
+      const scripts = await scriptCollection.findOne({ name: scriptName });
+      return scripts; // Return the result of the query
+    });
+  } catch (error) {
+    console.error('Error connecting and executing:', error.message);
+    throw error;
+  }
+}
+
 async function getScriptStateLogs() {
   try {
     return await connectAndExecute(async () => {
@@ -43,24 +55,25 @@ async function getScriptStateLogs() {
       if (!scripts || scripts.length === 0) {
         throw new Error('No scripts found');
       }
-
+      
       // Flatten logs from all scripts, sort by startTime, and map to the desired format
       const allLogs = scripts.flatMap(script => 
-        script.logs
-          .sort((a, b) => new Date(a.startTime) - new Date(b.startTime)) // Sort logs by startTime
-          .map(log => {
-            const startTime = new Date(log.startTime);
-            const endTime = new Date(log.endTime);
-            const duration = endTime - startTime; // Calculate duration in milliseconds
+        Array.isArray(script.logs) ? // Check if logs is an array
+          script.logs
+            .sort((a, b) => new Date(a.startTime) - new Date(b.startTime)) // Sort logs by startTime
+            .map(log => {
+              const startTime = new Date(log.startTime);
+              const endTime = new Date(log.endTime);
+              const duration = endTime - startTime; // Calculate duration in milliseconds
 
-            return {
-              ScriptName: script.name,
-              Start: startTime,
-              End: endTime,
-              Duration: duration > 0 ? duration : 0, // Ensure non-negative duration
-              Status: log.status,
-            };
-          })
+              return {
+                ScriptName: script.name,
+                Start: startTime,
+                End: endTime,
+                Duration: duration > 0 ? duration : 0, // Ensure non-negative duration
+                Status: log.status,
+              };
+            }) : [] // Return empty array if logs is not an array
       );
 
       return allLogs;
@@ -122,9 +135,35 @@ async function updateLogStatus(scriptName, logId, status, message) {
   }
 }
 
+async function logExecutionHistory(name, startTime, endTime, status, message){
+  try {
+    const historyData = {
+      status: status, // In progress
+      endTime: endTime,
+      startTime: startTime,
+      message:message
+    };
+    return connectAndExecute(async () => {
+      const scriptCollection = MongoDB.getCollection('ScriptState');
+      const result = await scriptCollection.findOneAndUpdate(
+        { name: name },
+        { $push: { execution_history: historyData } },
+        { returnDocument: 'after' } // Return the updated document
+      );
+      return result;
+    });
+  }catch (error) {
+    console.error('Error updating log status:', error.message);
+    throw error;
+  }
+
+}
+
 module.exports = {
   updateScriptStatus,
+  getScriptState,
   getScriptStateLogs,
   logScriptStart,
-  updateLogStatus
+  updateLogStatus,
+  logExecutionHistory
 };
